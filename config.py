8aede3ef22033721363ecc7cdcbbd09c994b9b92
Nikita Marchant <nikita.marchant@gmail.com>
2015-10-31 11:09:43 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+REDIS_HOST = 'rainbowdash.lan'
+REDIS_PORT = 6379
+
+try:
+    from local_config import *
+except ImportError:
+    pass
---
+++ feeder.py
@@ -0,0 +1,34 @@
+from redis import StrictRedis
+from config import REDIS_HOST, REDIS_PORT
+import subprocess
+import re
+
+ARP_REGEX = r"(?P<host>^\S+) \((?P<ip>(\d{1,3}\.){3}\d{1,3})\) at (?P<mac>((\d|[a-f]){2}:){5}(\d|[a-f]){2}) \[\w+\] on (?P<interface>\S+)$"
+ARP_REGEX = re.compile(ARP_REGEX)
+
+
+def get_redis():
+    return StrictRedis(REDIS_HOST, REDIS_PORT)
+
+
+def send_mac(client, maclist):
+    payload = ','.join(maclist)
+    client.setex('incubator_pamela', payload, 5 * 60)
+
+
+def get_mac(*interfaces):
+    stdout = subprocess.check_output(['sudo', 'arp', '-a'])
+    stdout = stdout.split('\n')
+    valid = filter(lambda x: "<incomplete>" not in x, stdout)
+    valid = filter(lambda x: x.strip() != "", valid)
+
+    out = []
+
+    for line in valid:
+        match = re.match(ARP_REGEX, line)
+        if match:
+            machine = match.groupdict()
+            if machine['interface'] in interfaces:
+                out.append(machine)
+
+    return out
