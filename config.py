c1d0287b93459c4113a45bd6cf328b6fdec35d44
Nikita Marchant <nikita.marchant@gmail.com>
2017-03-24 10:37:31 +0100

    Add hostname gathering

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ REDIS_PORT = 6379
 INTERFACES = ['eth0']
 PERIOD = 30
 EXPIRATION = 300
+MOCK = False
 
 try:
-    from local_config import *
+    from local_config import * # NOQA
 except ImportError:
     pass
--- feeder.py
+++ feeder.py
@@ -1,11 +1,14 @@
 from redis import StrictRedis
-from config import REDIS_HOST, REDIS_PORT, INTERFACES, PERIOD, EXPIRATION
+from config import REDIS_HOST, REDIS_PORT, INTERFACES, PERIOD, EXPIRATION, MOCK
 import subprocess
 import re
 from periodic import periodic
 
 ARP_REGEX = r"(?P<host>^\S+) \((?P<ip>(\d{1,3}\.){3}\d{1,3})\) at (?P<mac>((\d|[a-f]){2}:){5}(\d|[a-f]){2}) \[\w+\] on (?P<interface>\S+)$"
 ARP_REGEX = re.compile(ARP_REGEX)
+
+ANDROID_REGEX = r"^(?P<android>android)-(?P<id>[0-9a-fA-F]+)$"
+ANDROID_REGEX = re.compile(ANDROID_REGEX)
 
 
 def get_redis():
@@ -17,8 +20,17 @@ def send_mac(client, maclist):
     client.setex('incubator_pamela', EXPIRATION, payload)
 
 
+def send_hostnames(client, hosts_dict):
+    if hosts_dict:
+        client.hmset("incubator_pamela_hostnames", hosts_dict)
+
+
 def get_mac(*interfaces):
-    stdout = subprocess.check_output(['sudo', 'arp', '-a'])
+    if MOCK:
+        with open("arp.mock", "r") as m:
+            stdout = m.read()
+    else:
+        stdout = subprocess.check_output(['sudo', 'arp', '-a'])
     stdout = stdout.split('\n')
     valid = filter(lambda x: "<incomplete>" not in x, stdout)
     valid = filter(lambda x: x.strip() != "", valid)
@@ -35,11 +47,57 @@ def get_mac(*interfaces):
     return out
 
 
+def strip_suffix(s, suf):
+    if s.endswith(suf):
+        return s[:-len(suf)]
+    return s
+
+
+def strip_prefix(s, pre):
+    if s.startswith(pre):
+        return s[len(pre):]
+    return s
+
+
+def is_host(host):
+    if "?" in host:
+        return False
+    if len(host) < 3:
+        return False
+
+    return True
+
+
+def format_host(host):
+    host = strip_suffix(host, ".lan.urlab.be")
+    host = strip_suffix(host, ".lan")
+    host = strip_suffix(host, ".local")
+
+    host = strip_suffix(host, "iPodtouch")
+    host = strip_suffix(host, "-PC")
+    host = strip_suffix(host, "-pc")
+
+    host = strip_prefix(host, "pc-")
+    host = strip_prefix(host, "PC-")
+
+    if host.startswith("android"):
+        match = re.match(ANDROID_REGEX, host)
+        if match:
+            host = "unknown-android"
+        else:
+            host = strip_prefix(host, "android-")
+    return host
+
+
 @periodic(PERIOD)
 def main(client):
-    macdict = get_mac(*INTERFACES)
-    maclist = [x['mac'] for x in macdict]
+    macdicts = get_mac(*INTERFACES)
+
+    maclist = [x['mac'] for x in macdicts]
     send_mac(client, maclist)
+
+    hostnames = {x['mac']: format_host(x['host']) for x in macdicts if is_host(x['host'])}
+    send_hostnames(client, hostnames)
 
 if __name__ == '__main__':
     client = get_redis()
